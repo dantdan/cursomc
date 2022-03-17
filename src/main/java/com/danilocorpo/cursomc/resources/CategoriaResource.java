ae9438aace6b32f4ba2bdad825196820e01faf77
Danilo <danilo_lednik@hotmail.com>
2022-03-17 20:50:38 -0300

    Testando a chamada Rest

---
+++ src/main/java/com/danilocorpo/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.danilocorpo.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String Listar(){
+        return "Rest esta funcionando";
+    }
+}
