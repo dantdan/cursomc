3e7dc0f5f5cfa4edb6b022be74834849c3ef562d
Danilo <danilo_lednik@hotmail.com>
2022-05-27 08:55:07 -0300

    População de dados automatica na tabela Categoria

--- src/main/java/com/danilocorpo/cursomc/CursomcApplication.java
+++ src/main/java/com/danilocorpo/cursomc/CursomcApplication.java
@@ -1,13 +1,29 @@
 package com.danilocorpo.cursomc;
 
+import com.danilocorpo.cursomc.domain.Categoria;
+import com.danilocorpo.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+
+		repository.saveAll(Arrays.asList(cat1,cat2));
+	}
 }
