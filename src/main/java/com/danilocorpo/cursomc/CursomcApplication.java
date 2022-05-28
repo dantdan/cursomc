28549f71f8cbd96b7b0486c24b20af03566619c2
Danilo <danilo_lednik@hotmail.com>
2022-05-28 18:53:50 -0300

    entidade Produto criada

--- src/main/java/com/danilocorpo/cursomc/CursomcApplication.java
+++ src/main/java/com/danilocorpo/cursomc/CursomcApplication.java
@@ -1,7 +1,9 @@
 package com.danilocorpo.cursomc;
 
 import com.danilocorpo.cursomc.domain.Categoria;
+import com.danilocorpo.cursomc.domain.Produto;
 import com.danilocorpo.cursomc.repositories.CategoriaRepository;
+import com.danilocorpo.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,7 +15,9 @@ import java.util.Arrays;
 public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
-	CategoriaRepository repository;
+	CategoriaRepository categoriaRepository;
+	@Autowired
+	ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -24,6 +28,20 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
 
-		repository.saveAll(Arrays.asList(cat1,cat2));
+		Produto prod1 = new Produto(null,"Computador",2000.0);
+		Produto prod2 = new Produto(null,"Impressora",800.0);
+		Produto prod3 = new Produto(null,"Mouse",80.0);
+
+
+		cat1.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3));
+		cat2.getProdutos().addAll(Arrays.asList( prod2));
+
+		prod1.getCategorias().addAll(Arrays.asList(cat1));
+		prod2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		prod3.getCategorias().addAll(Arrays.asList(cat1));
+
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(prod1,prod2,prod3));
 	}
 }
--- src/main/java/com/danilocorpo/cursomc/domain/Produto.java
+++ src/main/java/com/danilocorpo/cursomc/domain/Produto.java
@@ -6,27 +6,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
 
-    // Versão 1 da classe do tipo Serializable
     private static final long seriaLVersionUID = 1l;
-
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+    private Double preco;
 
-    @ManyToMany(mappedBy = "categorias")
-    private List<Produto> produtos = new ArrayList<Produto>();
+    @ManyToMany
+    @JoinTable(name = "PRODUTO_CATEGORIA",
+        joinColumns = @JoinColumn(name = "produto_id"),
+        inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+    private List<Categoria> categorias = new ArrayList<Categoria>();
 
-    public Categoria(){
+    public Produto(){
+
     }
 
-    public Categoria(Integer id, String nome) {
+    public Produto(Integer id, String nome, Double preco) {
         this.id = id;
         this.nome = nome;
+        this.preco = preco;
     }
 
     public Integer getId() {
@@ -45,12 +48,20 @@ public class Categoria implements Serializable {
         this.nome = nome;
     }
 
-    public List<Produto> getProdutos() {
-        return produtos;
+    public Double getPreco() {
+        return preco;
     }
 
-    public void setProdutos(List<Produto> produtos) {
-        this.produtos = produtos;
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public List<Categoria> getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(List<Categoria> categorias) {
+        this.categorias = categorias;
     }
 
     @Override
